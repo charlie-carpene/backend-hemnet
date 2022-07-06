d32c05fdce569403c35a82804949104c6c591cb1
charlie-carpene <solunacisv@gmail.com>
2022-07-06 14:28:05 +0200

    clean routes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,19 +1,10 @@
 class MoviesController < ApplicationController
-  before_action :set_movie, only: [:show, :update, :destroy]
+  before_action :set_movie, only: :show
 
-  # GET /movies
-  def index
-    @movies = Movie.all
-
-    render json: @movies
-  end
-
-  # GET /movies/1
   def show
     render json: @movie
   end
 
-  # POST /movies
   def create
     @category = Category.where(name: movie_params[:category]).first
 
@@ -29,27 +20,11 @@ class MoviesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /movies/1
-  def update
-    if @movie.update(movie_params)
-      render json: @movie
-    else
-      render json: @movie.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /movies/1
-  def destroy
-    @movie.destroy
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_movie
       @movie = Movie.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def movie_params
       params.require(:movie).permit(:title, :year, :poster, :category)
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+Movie.destroy_all
 Category.destroy_all
 
 Category.create(name: "movie")
